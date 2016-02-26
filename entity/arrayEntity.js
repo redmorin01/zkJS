f559fa94dee19f46086fe039e2c3a93852f14b86
Roosevelt <edmorinroosevelt@gmail.com>
2016-02-26 15:50:48 +0100

    realisation arrayGet

--- entity/arrayEntity.js
+++ entity/arrayEntity.js
@@ -118,47 +118,41 @@ Array.prototype.getAt = function(param){
     return paramFunc ? paramFunc(this, param) : [];
 };
 
-
-
-
-
-
-
-
-
-
-var stringGetPath = "_ENTITY_._PARAMETERS_.string.get.";
+var arrayGetPath = "_ENTITY_._PARAMETERS_.array.get.";
 /**
  *  Pour un argument de type string :
  *  Renvoie le résultat dans un tableau
  */
-zk().setContainer(stringGetPath+"string", function(el, param){
-    var res = el.match(new RegExp(param, 'g'));
-    return res ? res : [];
+zk().setContainer(arrayGetPath+"string", function(el, param){
+  return zk().getContainer(arrayGetPath+"regexp")(el,new RegExp(param));
 });
 /**
  *  Pour un argument de type regexp :
  *  Renvoie le résultat dans un tableau
  */
-zk().setContainer(stringGetPath+"regexp", function(el, param){
-    (''+param).replace(/^\/(.*)\/([gi]*)$/, function(str, s1, s2){ param = new RegExp(s1, s2.trim("g")+"g") });
-    var res = el.match(param);
-    return res ? res : [];
+zk().setContainer(arrayGetPath+"regexp", function(el, param){
+    var res = [];
+    zk().toolbox().each(el,function(){
+        if(param.test(this.v)){
+            res.push(this.v);
+        }
+    });
+    return res;
 });
 /**
  * Pour un argument de type number :
  * - Renvoie les premiers résultats si l'argument <param> est positif
  * - Sinon renvoie les derniers éléments
  */
-zk().setContainer(stringGetPath + "number", function (el, param) { return ( param < 0 ) ? el.slice(param) : el.slice(0, param); });
+zk().setContainer(arrayGetPath + "number", function (el, param) { return ( param < 0 ) ? el.slice(param) : el.slice(0, param); });
 /**
  * Pour un argument de type array :
  * Le résulat est obtenu en fonction du type des éléments qui se trouve dans le tableau
  */
-zk().setContainer(stringGetPath + "array", function (el, param) {
+zk().setContainer(arrayGetPath + "array", function (el, param) {
     var res = [];
     zk().toolbox().each(param, function () {
-        var paramFunc = zk().getContainer(stringGetPath+zk().toolbox().is(this.v));
+        var paramFunc = zk().getContainer(arrayGetPath+zk().toolbox().is(this.v));
         if (paramFunc) {
             var r = paramFunc(el, this.v);
             if(r){ res = res.concat(r) }
@@ -167,8 +161,8 @@ zk().setContainer(stringGetPath + "array", function (el, param) {
     return res
 });
 Array.prototype.get = function(param){
-    if(param===undefined){ return "" }
-    var paramFunc = zk().getContainer("_ENTITY_._PARAMETERS_.string.get."+zk().toolbox().is(param));
+    if(param===undefined){ return [] }
+    var paramFunc = zk().getContainer("_ENTITY_._PARAMETERS_.array.get."+zk().toolbox().is(param));
     return paramFunc ? paramFunc(this, param) : "";
 };
 
